b6f8dec6a4644efecb9d06662ab4c95292b0fc3f
Algram <aliasgram@gmail.com>
2017-04-28 08:53:52 +0200

    Added back navigation and some elements

--- screens/EntryScreen.js
+++ screens/EntryScreen.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { Item, Input } from 'native-base';
+import {
+  View,
+  Text,
+  Keyboard
+} from 'react-native';
+import { Item, Input, Button } from 'native-base';
 
 
 export default class LogImageEntry extends Component {
   render() {
+    const { goBack } = this.props.navigation;
+
     return (
-      <Item regular>
-        <Input placeholder="Regular Textbox" />
-      </Item>
+      <View>
+        <Item regular>
+          <Input placeholder="Regular Textbox" />
+        </Item>
+        <Button
+          block
+          onPress={() => {
+            goBack();
+            Keyboard.dismiss();
+          }}
+        >
+          <Text>Done</Text>
+        </Button>
+      </View>
     );
   }
 }
