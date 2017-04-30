3376e7548f9be566c28aaa691f6129056d6ba9eb
Unknown <t.goehringer1995@web.de>
2017-04-30 16:46:00 +0200

    Removed comment

--- screens/EntryScreen.js
+++ screens/EntryScreen.js
@@ -162,7 +162,7 @@ export default class Entry extends Component {
               style={{ alignSelf: 'center' }}
               onPress={() => {
                 this.handleSubmit();
-                //goBack();
+                goBack();
                 Keyboard.dismiss();
               }}
             >
