14f7028e525207b47f1c59f0014360b1fe68010f
Algram <aliasgram@gmail.com>
2017-04-30 11:27:39 +0200

    Added correct iso time

--- services/databaseService.js
+++ services/databaseService.js
@@ -16,115 +16,115 @@ async function getEntries() {
           text: [
             {
               value: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               value: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
               emotion: 'neutral',
-              createdAt: '28.04.2017 16:34'
+              createdAt: '2017-04-28T16:34:42.732Z'
             },
             {
               emotion: 'happy',
-              createdAt: '28.04.2017 16:35'
+              createdAt: '2017-04-28T16:35:42.732Z'
             }
           ],
           badges: [
             {
               badgeType: 'diapers',
               value: '5',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'hydration',
               value: '1250ml',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'meals',
               value: '3',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'weight',
               value: '2500g',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             }
           ],
           milestone: false,
-          createdAt: '27.04.2017 16:33'
+          createdAt: '2017-04-27T15:00:42.732Z'
         },
         {
           text: [
             {
               value: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             }
           ],
           badges: [
             {
               badgeType: 'diapers',
               value: '5',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'hydration',
               value: '1050ml',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'headCircumference',
               value: '72cm',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'weight',
               value: '2700g',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'nursing',
               value: '4:38 min',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             }
           ],
           milestone: false,
-          createdAt: '28.04.2017 16:33'
+          createdAt: '2017-04-28T16:33:42.732Z'
         },
         {
           text: [
             {
               value: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               value: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-              createdAt: '28.04.2017 16:34'
+              createdAt: '2017-04-28T16:34:42.732Z'
             },
             {
               value: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-              createdAt: '28.04.2017 16:35'
+              createdAt: '2017-04-28T16:35:42.732Z'
             }
           ],
           badges: [
             {
               badgeType: 'diapers',
               value: '5',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'hydration',
               value: '1450ml',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'meals',
               value: '3',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             },
             {
               badgeType: 'height',
               value: '94cm',
-              createdAt: '28.04.2017 16:33'
+              createdAt: '2017-04-28T16:33:42.732Z'
             }
           ],
           milestone: false,
