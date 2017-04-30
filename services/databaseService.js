e93410f8608036c4ad8e2c9b662428edc77ff505
Unknown <t.goehringer1995@web.de>
2017-04-30 12:18:50 +0200

    Fixed route

--- services/databaseService.js
+++ services/databaseService.js
@@ -3,7 +3,7 @@ import { serverAPI } from '../config/api';
 
 async function getEntries() {
   try {
-    const response = await fetch(`${serverAPI}/entries`);
+    const response = await fetch(`${serverAPI}/quengel/entries`);
     return response.json();
   } catch (error) {
     console.error(error);
