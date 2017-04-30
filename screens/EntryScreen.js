cbbbb2f74a2d35a66516b6f07e3675b2fa1f7f02
Algram <aliasgram@gmail.com>
2017-04-30 14:22:57 +0200

    Added swiper for emotion

--- screens/EntryScreen.js
+++ screens/EntryScreen.js
@@ -2,12 +2,37 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
   Keyboard
 } from 'react-native';
-import { Item, Icon, Input, Button } from 'native-base';
+import { Item, Input, Button } from 'native-base';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 export default class Entry extends Component {
+  constructor() {
+    super();
+    this.state = {
+      emotion: 'help'
+    };
+  }
+
+  setEmotion(direction) {
+    const emotions = ['help', 'emoticon-sad', 'emoticon-neutral', 'emoticon-happy'];
+    const currEmotion = this.state.emotion;
+    let newEmotion;
+
+    if (direction === 'previous') {
+      newEmotion = emotions[emotions.indexOf(currEmotion) - 1];
+    } else if (direction === 'next') {
+      newEmotion = emotions[emotions.indexOf(currEmotion) + 1];
+    }
+
+    if (newEmotion) {
+      this.setState({ emotion: newEmotion });
+    }
+  }
+
   render() {
     const { goBack } = this.props.navigation;
 
@@ -22,8 +47,14 @@ export default class Entry extends Component {
           </View>
         </View>
         <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 0.5, backgroundColor: 'green' }}>
-            <Text>Test</Text>
+          <View style={{ flex: 0.5, flexDirection: 'row', backgroundColor: '#5363ab', alignItems: 'center', justifyContent: 'center' }}>
+            <TouchableOpacity onPress={() => this.setEmotion('previous')}>
+              <Icon name="chevron-left" size={40} color="#ffffff" />
+            </TouchableOpacity>
+            <Icon name={this.state.emotion} size={90} color="#ffffff" />
+            <TouchableOpacity onPress={() => this.setEmotion('next')}>
+              <Icon name="chevron-right" size={40} color="#ffffff" />
+            </TouchableOpacity>
           </View>
           <View style={{ flex: 0.5, backgroundColor: 'blue' }}>
             <Text>Test</Text>
