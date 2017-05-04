e83b55222a92c2b9e2307923be37d2e04e5489e7
Unknown <t.goehringer1995@web.de>
2017-05-04 10:55:38 +0200

    Added global styles

---
+++ config/globals.js
@@ -0,0 +1,26 @@
+module.exports = {
+  COLOR: {
+    DIAPERS: '#c27ba0',
+    HYDRATION: '#6fa8dc',
+    MEALS: '#93c47d',
+    NURSING: '#fe7d55',
+    MEASUREMENT: '#ffac49'
+  },
+  FONTSIZE: {
+    DISPLAY1: 34,
+    HEADLINE: 24,
+    SUBHEADING: 16,
+    BODY: 14,
+    CAPTION: 13
+  },
+  OPACITY: {
+    DISPLAY1: 0.54,
+    HEADLINE: 0.87,
+    SUBHEADING: 0.87,
+    BODY: 0.87,
+    CAPTION: 0.54
+  },
+  LAYOUT: {
+    PADDING: 16
+  }
+};
