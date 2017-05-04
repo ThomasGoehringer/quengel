b686eb40b115d0251dbe30a14d43f17219dd3609
Xuesong Zhang <xuesong@ahorngmbh.de>
2017-05-04 09:23:03 +0200

    Add content to the profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,30 +1,129 @@
 import React, { Component } from 'react';
 import {
-  View,
   Text
 } from 'react-native';
-import { Item, Icon, Input, Button, Badge, List, ListItem, Right, Switch } from 'native-base';
-
+import {
+  Thumbnail,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Modal,
+  View,
+  TextInput
+} from 'native-base';
+import {
+} from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class Profil extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalVisible: false
+    };
+  }
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
   render() {
-    const { goBack } = this.props.navigation;
 
     return (
-      <View style={{ flex: 1, justifyContent: 'space-between' }}>
+
+      <View style={{ flex: 1 }}>
+        <ListItem
+          style={{
+            justifyContent: 'center',
+            borderBottomWidth: 0
+          }}
+        >
+          <Text
+            style={{
+              textAlign: 'center'
+            }}
+          >
+            Dein kleiner quengel!
+          </Text>
+        </ListItem>
+        <ListItem
+          style={{
+            justifyContent: 'center',
+            borderBottomWidth: 0
+          }}
+        >
+          <Thumbnail
+            size={100}
+            source={require('../img/test.png')}
+          />
+        </ListItem>
         <List>
           <ListItem >
-            <Text>Einstellung 1</Text>
+            <Left>
+              <Text>Name</Text>
+            </Left>
+            <Body>
+              <Text>Quengel</Text>
+            </Body>
+            <Right>
+              <Icon
+                onClick={renderModalFenster}
+                style={{ color: '#6B6B6E' }}
+                name="lead-pencil"
+                size={20}
+              />
+            </Right>
+          </ListItem>
 
+          <ListItem >
+            <Left>
+              <Text>Geschlecht</Text>
+            </Left>
+            <Body>
+              <Text>Junge</Text>
+            </Body>
+            <Right>
+              <Icon
+                name="lead-pencil"
+                size={20}
+              />
+            </Right>
           </ListItem>
           <ListItem>
-            <Text>Einstellung 2</Text>
+            <Left>
+              <Text>Geburtstag</Text>
+            </Left>
+            <Body>
+              <Text>2017.4.18</Text>
+            </Body>
+            <Right>
+              <Icon
+                name="lead-pencil"
+                size={20}
+              />
+            </Right>
           </ListItem>
           <ListItem>
-            <Text>Einstellung 3</Text>
+            <Left>
+              <Text>E-Mail-Adresse</Text>
+            </Left>
+            <Body>
+              <Text>myqbaby@guengel.de</Text>
+            </Body>
+            <Right>
+            </Right>
           </ListItem>
         </List>
       </View>
+
     );
   }
 }
+
+// const BabyApp = StackNavigator({
+//   ProfileName: { screen: ProfileName }
+// }, { headerMode: 'screen' });
+//
+// AppRegistry.registerComponent('BabyApp', () => BabyApp);
