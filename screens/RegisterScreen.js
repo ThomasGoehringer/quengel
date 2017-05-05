2f430d8cf93bdbed21438282d13d328b468d0955
Algram <aliasgram@gmail.com>
2017-05-05 20:14:22 +0200

    More styling basics for the RegisterScreen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
+  StatusBar,
   View,
+  Text,
   Button,
   TextInput,
   Keyboard
 } from 'react-native';
 import databaseService from '../services/databaseService';
 import { setData } from '../services/storageService';
+import { COLOR } from '../config/globals';
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.PRIMARY
+  },
+  text: {
+    color: '#FFFFFF'
+  }
+});
 
 export default class LoginScreen extends Component {
   constructor(props) {
@@ -46,7 +60,15 @@ export default class LoginScreen extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor={COLOR.PRIMARY}
+          barStyle="light-content"
+        />
+      <Text style={styles.text}>
+          Deine Anmeldung
+          Melde dich einfach mit deiner E-Mail Adresse und einem Passwort bei quengel an.
+        </Text>
         <TextInput
           placeholder="E-Mail"
           onChangeText={email => this.setState({ email })}
@@ -62,7 +84,7 @@ export default class LoginScreen extends Component {
         <Button
           onPress={this.handleSubmit}
           title="Registrieren"
-          color="#841584"
+          color={COLOR.PRIMARY}
         />
       </View>
     );
