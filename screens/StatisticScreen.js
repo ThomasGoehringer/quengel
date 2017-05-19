0619da73b782500b873dc6f89592a9aa3de42def
Algram <aliasgram@gmail.com>
2017-05-19 10:16:02 +0200

    Example chart

--- screens/StatisticScreen.js
+++ screens/StatisticScreen.js
@@ -9,7 +9,10 @@ import {
   VictoryTheme,
   VictoryChart,
   VictoryLine,
-  VictoryArea
+  VictoryArea,
+  VictoryGroup,
+  VictoryBar,
+  VictoryScatter
 } from 'victory-native';
 
 export default class StatisticScreen extends Component {
@@ -18,30 +21,34 @@ export default class StatisticScreen extends Component {
   }
 
   render() {
+    const data = [
+      {lineY: 4, x: 1, y: 10, _y0: 3},
+      {lineY: 4, x: 2, y: 5, _y0: 2},
+      {lineY: 4, x: 3, y: 3, _y0: 1},
+      {lineY: 4, x: 4, y: 5, _y0: 2},
+      {lineY: 4, x: 5, y: 2, _y0: 1},
+      {lineY: 4, x: 6, y: 4, _y0: 2},
+      {lineY: 4, x: 7, y: 5, _y0: 2}
+    ];
+
     return (
       <ScrollView>
         <Text>Screen Statistics</Text>
-        <VictoryChart theme={VictoryTheme.material}>
-          <VictoryArea theme={VictoryTheme.material}
-            data={[
-              { month: 'September', profit: 35000, loss: 2000 },
-              { month: 'October', profit: 42000, loss: 8000 },
-              { month: 'November', profit: 55000, loss: 5000 }
-            ]}
-            x="month"
-            y={datum => datum.profit - datum.loss}
-          />
-        </VictoryChart>
-        <VictoryChart theme={VictoryTheme.material}>
-          <VictoryLine theme={VictoryTheme.material}
-            data={[
-              {month: "September", profit: 35000, loss: 2000},
-              {month: "October", profit: 42000, loss: 8000},
-              {month: "November", profit: 55000, loss: 5000}
-            ]}
-            x="month"
-            y={(datum) => datum.profit - datum.loss}
-          />
+        <VictoryChart>
+          <VictoryGroup
+            data={data}
+            y={(d) => (d.lineY)}
+          >
+            <VictoryLine style={{ data: { stroke: "red" } }}/>
+            <VictoryScatter
+              size={3}
+              style={{ data: { stroke: "navy", fill: "white", strokeWidth: 2 } }}
+            />
+          </VictoryGroup>
+          <VictoryArea
+             data={data}
+             style={{ data: { fill: "navy", opacity: 0.4 } }}
+           />
         </VictoryChart>
       </ScrollView>
     );
